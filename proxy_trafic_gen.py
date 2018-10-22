c2b367cbfd87fe0250ed2b6939f6008923fe3a63
Saipranav Koyyada <ksaipranav@gmail.com>
2018-10-22 21:50:21 +0530

    Update proxy_trafic_gen.py

--- proxy_trafic_gen.py
+++ proxy_trafic_gen.py
@@ -1,29 +1,34 @@
 #dntfury
+#SaipranavK
                             
-      
 import requests
 from itertools import cycle
 import traceback
+
 #PASTE PROXY HERE
 proxies = [
-'0.0.0.0:8080',
-'ip:port',
-'ip:port'
-
+'0.0.0.0:8080',' ',' ',' ',
+#Formatting-'ip:port',
 ]
+
 #proxies = get_proxies()
+
 proxy_pool = cycle(proxies)
  
 url = 'http://www.example.com/'
 
 for i in range(1,50):
+    
     #Get a proxy from the pool
+    
     proxy = next(proxy_pool)
     print("Request #%d"%i)
+    
     try:
         response = requests.get(url,proxies={"http": proxy, "https": proxy})
         print("REQUESTED VIA:",proxy)
         print(response)
+    
     except:
         #When proxy will get connection errors. 
         # just leave that  
